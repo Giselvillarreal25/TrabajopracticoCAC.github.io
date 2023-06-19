9b402a726bcda24a25a03bf5cb8f77020de31608
Giselvillarreal25 <131629007+Giselvillarreal25@users.noreply.github.com>
2023-06-19 03:25:49 -0300

    Update compratickets.js

--- compratickets.js
+++ compratickets.js
@@ -1,10 +1,11 @@
-let botonBorrar = document.getElementById("button--borrar");
-let inputs = document.querySelectorAll("input");
-let categorias = document.getElementById("Categoria");
-let botonResumen = document.getElementById("button--resumen");
 let estudiante = document.getElementById("descuento1");
 let trainee = document.getElementById("descuento2");
 let junior = document.getElementById("descuento3");
+let inputs = document.querySelectorAll("input");
+let categorias = document.getElementById("Categoria");
+let botonBorrar = document.getElementById("button--borrar");
+let botonResumen = document.getElementById("button--resumen");
+
 
 
 estudiante.addEventListener('click', ()=>{
@@ -23,7 +24,7 @@ estudiante.addEventListener('click', ()=>{
 trainee.addEventListener('click', ()=>{
     document.getElementById("Categoria").selectedIndex="2";
     trainee.style.backgroundColor = 'rgb(151, 240, 235)';
-    trainee.style.color = 'whitesmoke';
+    trainee.style.color = 'black';
     estudiante.style.backgroundColor = 'transparent';
     estudiante.style.color = 'black';
     junior.style.backgroundColor = 'transparent';
@@ -34,7 +35,7 @@ trainee.addEventListener('click', ()=>{
 junior.addEventListener('click', ()=>{
     document.getElementById("Categoria").selectedIndex="3";
     junior.style.backgroundColor = 'rgb(243, 241, 109)';
-    junior.style.color = 'whitesmoke';
+    junior.style.color = 'black';
     estudiante.style.backgroundColor = 'transparent';
     estudiante.style.color = 'black';
     trainee.style.backgroundColor = 'transparent';
@@ -45,33 +46,33 @@ junior.addEventListener('click', ()=>{
 categorias.addEventListener('click', ()=>{
     valorCategorias = categorias.value;
     if(valorCategorias == 1){
-        cartaA.style.backgroundColor = 'rgb(41, 41, 190)';
-        cartaA.style.color = 'whitesmoke';
-        cartaB.style.backgroundColor = 'transparent';
-        cartaB.style.color = 'black';
-        cartaC.style.backgroundColor = 'transparent';
-        cartaC.style.color = 'black';
+        estudiante.style.backgroundColor = 'rgb(41, 41, 190)';
+        estudiante.style.color = 'black';
+        trainee.style.backgroundColor = 'transparent';
+        trainee.style.color = 'black';
+        junior.style.backgroundColor = 'transparent';
+        junior.style.color = 'black';
         promocion = 0.2;
     }
     if(valorCategorias == 0){
         promocion = 1;
     }
     if(valorCategorias == 2){
-        cartaB.style.backgroundColor = 'green';
-        cartaB.style.color = 'whitesmoke';
-        cartaA.style.backgroundColor = 'transparent';
-        cartaA.style.color = 'black';
-        cartaC.style.backgroundColor = 'transparent';
-        cartaC.style.color = 'black';
+        trainee.style.backgroundColor = 'rgb(151, 240, 235)';
+        trainee.style.color = 'black';
+        estudiante.style.backgroundColor = 'transparent';
+        estudiante.style.color = 'black';
+        junior.style.backgroundColor = 'transparent';
+        junior.style.color = 'black';
         promocion = 0.5;
     }
     if(valorCategorias == 3){
-        cartaC.style.backgroundColor = 'goldenrod';
-        cartaC.style.color = 'whitesmoke';
-        cartaA.style.backgroundColor = 'transparent';
-        cartaA.style.color = 'black';
-        cartaB.style.backgroundColor = 'transparent';
-        cartaB.style.color = 'black';
+        junior.style.backgroundColor = 'rgb(243, 241, 109)';
+        junior.style.color = 'black';
+        estudiante.style.backgroundColor = 'transparent';
+        estudiante.style.color = 'black';
+        trainee.style.backgroundColor = 'transparent';
+        trainee.style.color = 'black';
         promocion = 0.85;
     }
 })
@@ -82,19 +83,19 @@ botonBorrar.addEventListener('click', ()=>{
     document.getElementById('total').innerHTML = 'Total a Pagar: $ ';
 });
 
-function nombreComprador(nombre){
+function comprador_nombre(nombre){
     compradorNombre = nombre;    
 }
 
-function apellidoComprador(apellido){
+function comprador_apellido(apellido){
     compradorApellido = apellido;    
 }
 
-function correoComprador(correo){
+function comprador_correo(correo){
     compradorCorreo = correo;    
 }
 
-function cantidadBoletos(valor){
+function cantidadTickets(valor){
     if(valor < 0){
         alert("La cantidad de boletos no puede ser nulo o menor a 0");
     }
